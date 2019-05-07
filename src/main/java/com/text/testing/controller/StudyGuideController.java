27bfb7308d2e023420b6bcb97f257cb23623bb5e
hemme82 <davidhemmesch@gmail.com>
2019-05-07 09:29:04 -0500

    added getmapping to StudyGuideController for working root(/)

--- src/main/java/com/text/testing/controller/StudyGuideController.java
+++ src/main/java/com/text/testing/controller/StudyGuideController.java
@@ -1,13 +1,29 @@
 package com.text.testing.controller;
 
+import com.text.testing.model.Topic;
+import com.text.testing.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class StudyGuideController {
 
+    @Autowired
+    private TopicService topicService;
+
+
     @GetMapping("/")
-    public String rootView() {
-        return "index";
+    public String rootView(ModelMap modal) {
+
+
+            List<Topic> topics = (List<Topic>) topicService.getAllTopics();
+
+            modal.addAttribute("topics", topics);
+            return "index";
+
     }
 }
